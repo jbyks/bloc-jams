afc584aa72a611a9ba02088b52a935baf7c70b86
jbyks <jochen.boeykens@gmail.com>
2016-10-18 10:06:05 -0400

    assignment-19-jquery-next-previous: completed

--- scripts/album.js
+++ scripts/album.js
@@ -20,7 +20,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         if (currentlyPlayingSongNumber !== songNumber) {
             // Switch from Play -> Pause button to indicate new song is playing.
             $(this).html(pauseButtonTemplate);
-            currentlyPlayingSongNumber = songNumber;
+            setSong(songNumber);
             currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
             updatePlayerBarSong();
         } else if (currentlyPlayingSongNumber === songNumber) {
@@ -169,12 +169,8 @@ var previousSong = function() {
 };
 
 var setSong = function(songNumber){
-    currentlyPlayingSongNumber = songNumber;
-    currentSongFromAlbum = if(songNumber != null){
-        album.songs[songNumber].title;
-    }else{
-        currentSongFromAlbum = null;
-    }
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber -1];
 };
 
 var getSongNumberCell = function(number){
